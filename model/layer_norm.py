8ce698217797b551d8c6543da8975f15ad5e005a
Jing Hua <tohjinghua123@gmail.com>
2023-05-29 00:22:40 +0800

    rms layer norm

--- model/layer_norm.py
+++ model/layer_norm.py
@@ -4,16 +4,25 @@ from typing import Dict
 
 
 # Layer normalization: https://arxiv.org/abs/1607.06450
-def fwd_layer_norm(params: Dict,
-                   x: jnp.ndarray,
-                   eps: float = 1e-6) -> jnp.ndarray:
-    weight = params['weight']
-    bias = params.get('bias', None)
+def fwd_layer_norm(params: Dict, x: jnp.ndarray, eps: float = 1e-6) -> jnp.ndarray:
+    weight = params["weight"]
+    bias = params.get("bias", None)
 
     mean = x.mean(-1, keepdims=True)
     var = x.var(-1, keepdims=True)
 
     y = (x - mean) / jnp.sqrt(var + eps) * weight
-    if bias is not None: y = y + bias
+    if bias is not None:
+        y = y + bias
+
+    return y
+
+
+# Root Mean Square Layer Normalization: https://arxiv.org/abs/1910.07467
+# Does not subtract mean and does not have bias
+def fwd_layer_norm_rms(params: Dict, x: jnp.ndarray, eps: float = 1e-6) -> jnp.ndarray:
+    weight = params["weight"]
+    var = x.var(-1, keepdims=True)
+    y = x / jnp.sqrt(var + eps) * weight
 
     return y
--- tests/layer_norm.py
+++ tests/layer_norm.py
@@ -11,18 +11,18 @@ keys = random.split(key, 3)
 
 # set up params, x and epsilon
 eps = 1e-5
-weight = random.uniform(keys[0], shape=(10, ))
-bias = random.uniform(keys[1], shape=(10, ))
+weight = random.uniform(keys[0], shape=(10,))
+bias = random.uniform(keys[1], shape=(10,))
 x = random.uniform(keys[2], shape=(3, 4, 10))
 
 params = {"weight": weight, "bias": bias}
 params_flax = {"params": {"scale": weight, "bias": bias}}
 
-# my linear norm output
+# my layer norm output
 output = fwd_layer_norm(params, x, eps)
 
-# flax linear norm output
+# flax layer norm output
 model = flax.linen.LayerNorm(epsilon=eps)
 output_flax = model.apply(params_flax, x)
 
-assert jnp.allclose(output, output_flax)
+assert jnp.allclose(output, output_flax) == True
