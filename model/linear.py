38687ec95066d1bc5e103ea5750d9871dbcaa386
Jing Hua <tohjinghua123@gmail.com>
2023-05-31 14:50:49 +0800

    init linear

--- model/linear.py
+++ model/linear.py
@@ -1,13 +1,25 @@
-from typing import Dict, List
+from typing import Dict
 
+import jax.nn as jnn
 import jax.numpy as jnp
+from jax import random
 
 
 def fwd_linear(params: Dict, x: jnp.ndarray) -> jnp.ndarray:
-    kernel = params['kernel']
-    bias = params.get('bias', None)
+    kernel = params["kernel"]
+    bias = params.get("bias", None)
 
     y = jnp.dot(x, kernel)
-    if bias is not None: y += bias
+    if bias is not None:
+        y += bias
 
     return y
+
+
+def init_linear(features: int, use_bias: bool = False, seed: int = 2418):
+    initializer = jnn.initializers.normal(1)
+    key = random.PRNGKey(seed)
+    params = {"kernel": initializer(key, (features,))}
+    if use_bias:
+        params["bias"] = jnp.zeros((features,))
+    return params
